482e8adba705ea623430c71d5ac7986583b76e79
Gustavo Gomez <gustavogomez092@gmail.com>
2026-03-30 03:35:45 -0700

    Use ShadowDark logo SVG as favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,6 +12,7 @@ export const Route = createRootRoute({
     ],
     links: [
       { rel: 'stylesheet', href: appCss },
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
     ],
   }),
   component: RootDocument,
